07db2531ccb11545d901f442066bce1c59efa2a6
redhawkdev <ddiego_2vini@hotmail.com>
2023-12-17 18:37:25 -0400

    Atualização de Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.scss";
 import Navbar from "@/components/main/Navbar";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="pt-br" suppressHydrationWarning>
       <body cz-shortcut-listen="true" className={inter.className}>
         <SpeedInsights />
+        <Analytics />
         <Navbar />
         {children}
       </body>
