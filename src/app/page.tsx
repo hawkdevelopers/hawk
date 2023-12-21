4f3271bfc7cb6ad85d4a89da9a55c527a63eeaca
redhawkdev <ddiego_2vini@hotmail.com>
2023-12-21 19:27:19 -0400

    Nova implementação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Hero from "./components/main/hero/Hero";
+import Hero from "../components/main/hero/Hero";
 
 export default function Home() {
   return (
